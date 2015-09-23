3498fa42c6cfe9eb82e672bed850a697a6af7ace
Gladys Maniago <gfmaniago@gmail.com>
2015-09-23 18:22:27 -0500

    scripts almost working

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,35 @@
 'use strict';
+var Backbone = require('backbone');
+var _ = require('backbone/node_modules/underscore');
+
+var toDoModel = require('./models/toDoModel.js');
+var toDoView= require('./views/toDoView.js');
+var toDoCollection= require('./collections/toDoCollection.js');
+
+$(document).ready(function() {
+
+	var $appForm = $('#appForm');
+	var $appInput = $('#appInput');
+	var appTemplate = _.template($('#list-row').html());
+	var $appList = $('#appList');
+
+	var newItems = new toDoCollection();
+
+	$appForm.on('submit', function(e){
+		e.preventDefault();
+		newItems.add({
+			item: $appInput.val()
+		})	
+	});
+	newItems.on('add', function(newListItem){
+		newListItem.save();
+		var newHtml = appTemplate(newListItem.toJSON());
+		$appList.append(newHtml);
+	});
+
+	$app.on('submit', onFormSubmit);
+
+	newItems.on('add', newListItem);
+	newItems.fetch();
+
+});
---
+++ scripts/views/toDoView.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var Backbone = require('backbone');
+var _ = require ('backbone/node_modules/underscore');
+var toDoModel = require('../models/toDoModel.js');
